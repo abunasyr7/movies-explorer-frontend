bc4ffed54ef635dbe0ae6763c30af661892704a6
abunasyr7 <abunasyr7@yandex.kz>
2022-09-19 22:45:38 +0600

    feat: add buttons in top of images

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -9,7 +9,9 @@ function MoviesCard({url, title, duration}) {
     return(
         <section className="movies-card">
             <div className="movies-card__container">
-                <img className="movies-card__img" src={url} alt={title} />
+                <div className="movies-card__icon"></div>
+                <div className="movies-card__save">Сохранить</div>
+                <img className="movies-card__img" src={url} alt={title}/>
                 <div className="movies-card__group">
                     <h2 className="movies-card__title">{title}</h2>
                     <div className="movies-card__duration-cnt">
